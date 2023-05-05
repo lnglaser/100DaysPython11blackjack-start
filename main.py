9b030045b26872f735712c5578917eb28f3b04a9
Leon Glaser <leon.glaser@gmail.com>
2023-05-05 14:47:18 -0400

    For loop to deal out 2 cards

--- main.py
+++ main.py
@@ -40,9 +40,15 @@ def deal_card():
     cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
     card = random.choice(cards)
     return (card)
+
+
 # Hint 5: Deal the user and computer 2 cards each using deal_card() and append().
-#user_cards = []
-#computer_cards = []
+user_cards = []
+computer_cards = []
+
+for _ in range(2):
+    user_cards.append(deal_card())
+    computer_cards.append(deal_card())
 
 # Hint 6: Create a function called calculate_score() that takes a List of cards as input
 # and returns the score.
