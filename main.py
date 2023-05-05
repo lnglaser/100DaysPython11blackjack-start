352498a6793401eaf4058b721a2f72d9d416526f
Leon Glaser <leon.glaser@gmail.com>
2023-05-05 14:38:07 -0400

    begin deal_card function

--- main.py
+++ main.py
@@ -12,8 +12,8 @@
 # The Jack/Queen/King all count as 10.
 # The the Ace can count as 11 or 1.
 # Use the following list as the deck of cards:
-## cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 # The cards in the list have equal probability of being drawn.
+#
 # Cards are not removed from the deck as they are drawn.
 # The computer is the dealer.
 
@@ -33,8 +33,12 @@
 
 # Hint 4: Create a deal_card() function that uses the List below to *return* a random card.
 # 11 is the Ace.
-#cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+import random
 
+
+def deal_card():
+    cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+    random.choice
 # Hint 5: Deal the user and computer 2 cards each using deal_card() and append().
 #user_cards = []
 #computer_cards = []
